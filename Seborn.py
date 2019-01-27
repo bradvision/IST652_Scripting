8fef447d98c414ae32b8738e8143b2a5e614a77f
bchoi <rexbradley.c@gmail.com>
2019-01-27 11:01:45 -0500

    Create Seborn.py
    Basic vizualisation codes from tutorials regarding Seaborn and Matplotlib

---
+++ Seborn.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+#IST 652
+#Bradley W. S. Choi
+#Jen Yeu Lo
+# Seaborn Presentation codes
+
+
+#########
+#Matplotlib
+####################
+#Import necessary libraries
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
+
+#######################
+# Import necessary libraries
+import matplotlib 
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(19680801)
+
+# example data
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+x = mu + sigma * np.random.randn(437)
+
+num_bins = 50
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, density=1)
+
+# add a 'best fit' line
+y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+     np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+ax.plot(bins, y, '--')
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
+#############
+#Seaborn
+#############################
+# Import necessary libraries
+import pandas as pd
+import seaborn as sns
+sns.set(style="whitegrid", palette="muted")
+
+# Load the example iris dataset
+iris = sns.load_dataset("iris")
+
+# "Melt" the dataset to "long-form" or "tidy" representation
+iris = pd.melt(iris, "species", var_name="measurement")
+
+# Draw a categorical scatterplot to show each observation
+sns.swarmplot(x="measurement", y="value", hue="species",
+              palette=["r", "c", "y"], data=iris)
+
+
+######################
+# Import necessary libraries
+import seaborn as sns
+sns.set(style="ticks")
+
+# Load the example dataset for Anscombe's quartet
+df = sns.load_dataset("anscombe")
+
+# Show the results of a linear regression within each dataset
+sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
+           col_wrap=2, ci=None, palette="muted", height=4,
+           scatter_kws={"s": 50, "alpha": 1})
+
+# Import Matplotlib
+import matplotlib.pyplot as plt
+
+# Check the available styles
+plt.style.available
+
+# Use Matplotlib defaults
+plt.style.use("classic")
+
+##########################
+
+# Import necessary libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load iris data
+iris = sns.load_dataset("iris")
+
+# Construct iris plot
+sns.swarmplot(x="species", y="petal_length", data=iris)
+
+# Show plot
+plt.show()
+
+###########################
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+# Load the brain networks example dataset
+df = sns.load_dataset("brain_networks", header=[0, 1, 2], index_col=0)
+
+# Select a subset of the networks
+used_networks = [1, 5, 6, 7, 8, 12, 13, 17]
+used_columns = (df.columns.get_level_values("network")
+                          .astype(int)
+                          .isin(used_networks))
+df = df.loc[:, used_columns]
+
+# Create a categorical palette to identify the networks
+network_pal = sns.husl_palette(8, s=.45)
+network_lut = dict(zip(map(str, used_networks), network_pal))
+
+# Convert the palette to vectors that will be drawn on the side of the matrix
+networks = df.columns.get_level_values("network")
+network_colors = pd.Series(networks, index=df.columns).map(network_lut)
+
+# Draw the full plot
+sns.clustermap(df.corr(), center=0, cmap="vlag",
+               row_colors=network_colors, col_colors=network_colors,
+               linewidths=.75, figsize=(13, 13))
+
+
+
+
+
+
+
+
+
